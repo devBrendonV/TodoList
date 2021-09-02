8071b868459a8569731b9d1179f06bd47b8b1c62
Brendon Victor <devbrendonvs@gmail.com>
2021-09-01 23:30:23 -0300

    Diretorios arrumados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Content from './components/Content';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 const App = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Content></Content>
         </div>
     )
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
+import Home from "./views/Home";
+import TodoList from "./views/TodoList";
 const Content = () => {
   return (
     <div>
