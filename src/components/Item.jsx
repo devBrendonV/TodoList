5081529f6bf5c18b90fb1ee3729af92cefb8623d
Brendon Victor <devbrendonvs@gmail.com>
2021-09-02 00:21:42 -0300

    HTML / CSS Component Item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import "./Item.css";
 const Item = () => {
   return (
-    <div>
-      <div>
-        <label htmlFor="">Todo
+    <div className="principal">
+      <div className="mensagem">
+        <span>
+          <strong>Sucesso!</strong> Tarefa criada
+        </span>
+      </div>
+      <div className="adicionarTarefa">
+        <InputGroup className="mb-3">
+          <Form.Control aria-describedby="basic-addon2" />
+          <Button id="basic-addon2">Add</Button>
+        </InputGroup>
+      </div>
+      <div className="itens">
+        <div className='item'>
+        <label htmlFor="">
           <input type="checkbox" />
+          <span>Nome Item</span>
         </label>
-        <Button variant="danger">Delete</Button>
-        <Button variant="warning">Edit</Button>
+        <div className="botoes">
+          <Button variant="danger">Deletar</Button>
+          <Button variant="success">Editar</Button>
+        </div>
+        
+        </div>
+        
       </div>
     </div>
   );
